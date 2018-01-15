6f92605e71d44359ca51288bca78c674326e3b2b
spencerdodd <spencer.evan.dodd@gmail.com>
2018-01-15 18:27:45 -0500

    continuing setup

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-    "fmt"
-
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/ec2"
+	"os"
+	"fmt"
+	/*
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	//*/
 )
+
+func initialize_aws() {
+	// configure your aws region
+	//sess, err := session.NewSession(&aws.Config{Region: aws.String("us-east-2")})
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("[!] please specify a runmode\n")
+		fmt.Printf("\t[-] exiting\n")
+		os.Exit(1)
+	}
+	fmt.Printf("[*] welcome to cuttlefish\n")
+	
+}
