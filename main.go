f344905bff12e72e47c73eb40c2e2e97839a8492
spencerdodd <spencer.evan.dodd@gmail.com>
2018-01-15 19:54:09 -0500

    credential loading

--- main.go
+++ main.go
@@ -12,9 +12,38 @@ import (
 	//*/
 )
 
-func initialize_aws() {
+type creds struct {
+	AccessKey string
+	SecretKey string
+}
+
+// getCreds()
+//	pull aws creds from creds.json
+func getCreds() creds {
+	fmt.Printf("[*] loading aws credentials from ./creds.json\n")
+	rawjson, err := ioutil.ReadFile("./creds.json")
+	if err != nil {
+		fmt.Println("\t[-]", err.Error())
+		os.Exit(1)
+	}
+	var awscreds creds
+	err = json.Unmarshal(rawjson, &awscreds)
+	if err != nil {
+		fmt.Println("\t[-]", err.Error())
+		os.Exit(1)
+	}
+	if len(awscreds.AccessKey) == 0 || len(awscreds.SecretKey) == 0 {
+		fmt.Println("\t[-] keys not loaded successfully")
+		os.Exit(1)
+	}
+	fmt.Printf("[+] creds loaded successfully\n")
+	fmt.Printf("\taccess_key: %v\n\tsecret_key: %v\n", awscreds.AccessKey, awscreds.SecretKey)
+	return awscreds
+}
+
+func initializeAWS() {
 	// load the creds
-	
+	getCreds()
 	// configure your aws region
 	//sess, err := session.NewSession(&aws.Config{Region: aws.String("us-east-2")})
 }
@@ -26,5 +55,10 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Printf("[*] welcome to cuttlefish\n")
-
+	initializeAWS()
 }
+
+
+
+
+
