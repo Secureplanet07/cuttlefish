1bce3a8931a3506700c8dffe66bfe73cede5fe24
spencerdodd <spencer.evan.dodd@gmail.com>
2018-01-23 15:45:56 -0500

    fixes to install script

--- install.sh
+++ install.sh
@@ -5,11 +5,9 @@ echo "[*] checking if golang is installed"
 if ! [[ -x "$(command -v go)" ]]
 then
 	echo "[!] golang not installed, installing golang to /usr/local/"
-	cd ~/Downloads
-	curl -O 'https://dl.google.com/go/go1.9.3.linux-amd64.tar.gz'
-	tar -C /usr/local -xzf go1.9.3.linux-amd64.tar.gz
+	curl -O 'https://dl.google.com/go/go1.9.3.linux-amd64.tar.gz' ~/Downloads/go1.9.3.linux-amd64.tar.gz
+	tar -C /usr/local -xzf ~/Downloads/go1.9.3.linux-amd64.tar.gz
 	export PATH="$PATH:/usr/local/go/bin"
-
 fi
 echo "[+] golang is installed!"
 
@@ -20,5 +18,6 @@ go get -u github.com/wayneashleyberry/terminal-dimensions
 echo "[*] compiling project"
 go build
 echo "[*] linking to /usr/local/bin"
+export PATH="$PATH:/usr/local/bin"
 ln -s $(pwd)/cuttlefish /usr/local/bin/cuttlefish 
 echo "[+] installation complete!"
