d4ba5895896414747da78e0f3ef498f3a21a1730
spencerdodd <spencer.evan.dodd@gmail.com>
2018-01-25 21:04:12 -0500

    fixed header to show all supported enum services

--- main.go
+++ main.go
@@ -1043,12 +1043,16 @@ func main() {
 	cuttle_header_2 := "           O  o `- o ,-`           `.o `._./            "
 	cuttle_header_3 := "o   O   o   o _O  o /   cuttlefish   \\ O  o    O   o   O"
 	cuttle_header_4 := "__o___O____,-`  `\\_*         v0.0     \\._____o___coastal"
-	cuttle_header_5 := "[~] supports: ssh, smtp, ftp, http, https, smb, ms-sql"
+	cuttle_header_5 := "[~] supports identification and enumeration of: "
+	cuttle_header_6 := "\tftp, ssh, telnet, smtp, snmp, ftp, http, https"
+	cuttle_header_7 := "\tsmb, ms-sql, rdp"
 	colorPrint(cuttle_header_1, string_format.blue,logging, true)
 	colorPrint(cuttle_header_2, string_format.blue,logging, true)
 	colorPrint(cuttle_header_3, string_format.blue,logging, true)
 	colorPrint(cuttle_header_4, string_format.blue,logging, true)
 	colorPrint(cuttle_header_5, string_format.blue,logging, true)
+	colorPrint(cuttle_header_6, string_format.blue,logging, true)
+	colorPrint(cuttle_header_7, string_format.blue,logging, true)
 	if *testing {
 		colorPrint("[*] running in test mode", string_format.yellow, logging, true)
 	}
