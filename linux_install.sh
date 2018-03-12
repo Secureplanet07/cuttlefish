7686a6fdcbe6bd41431a0e1c5004b406142ee0e6
spencerdodd <spencer.evan.dodd@gmail.com>
2018-03-12 17:28:27 -0400

    install changes

--- linux_install.sh
+++ linux_install.sh
@@ -12,20 +12,21 @@ then
 	curl -O 'https://dl.google.com/go/go1.9.3.linux-amd64.tar.gz'
 	sudo tar -C /usr/local -xzf ~/Downloads/go1.9.3.linux-amd64.tar.gz
 	echo "[*] adding go binaries to PATH"
-	echo 'PATH="$PATH:/usr/local/go/bin"' >> ~/.bashrc
+	echo 'export PATH="$PATH:/usr/local/go/bin"' >> ~/.bashrc
 	source ~/.bashrc
 	echo "[*] PATH: $PATH"
 fi
 echo "[+] golang is installed!"
 cd $INSTALLDIR
 echo "[*] changing gopath to current dir"
-export GOPATH=$INSTALLDIR
+echo 'export GOPATH=$INSTALLDIR' >> ~/.bashrc
+source ~/.bashrc
 echo "[*] installing go dependencies"
 go get -u github.com/wayneashleyberry/terminal-dimensions
 echo "[*] compiling project"
 go build
 echo "[*] linking to /usr/local/bin"
-echo 'PATH="$PATH:/usr/local/bin"' >> ~/.bashrc
+echo 'export PATH="$PATH:/usr/local/bin"' >> ~/.bashrc
 source ~/.bashrc
 echo "[*] PATH: $PATH"
 ln -s $INSTALLDIR/cuttlefish /usr/local/bin/cuttlefish 
@@ -42,5 +43,5 @@ echo "	[*] go binaries: $(which go)"
 echo "	[*] GOPATH: $GOPATH"
 echo "	[*] PATH: $PATH"
 echo "	[*] cuttlefish: $(which cuttlefish)"
-echo "	[*] SecLists: $(ls ~/Documents/tools/SecLists)"
+echo -e "	[*] SecLists: \n$(for dir in $(ls ~/Documents/tools/SecLists); do echo '		$dir'; done)"
 echo "[+] installation complete!"
